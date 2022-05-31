c1810b763bd0384ad003f044dc731cdb0706bd01
eGGrah <106174841+eGGrah@users.noreply.github.com>
2022-05-31 19:57:28 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -34,12 +34,22 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), 'element present'
 
-
+        
 @pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    links = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, links)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.message_name_coincides_product_name()
+    page.cost_of_product_coincides_with_basket()
+    
+
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket2(browser, link):
     links = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, links)
     page.open()
